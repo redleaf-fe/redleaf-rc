3faa89221d400c9a311a528c0744d36bf947836b
zhufeng <vzhufeng@163.com>
2021-03-12 20:30:44 +0800

    fix: 各个组件的props类型从index中导出

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // 组件
 export { default as Bubble } from './bubble';
 export { default as Button } from './button';
+export { default as Check } from './check';
 export { default as ConfigProvider } from './config-provider';
 export { default as DateTime } from './date-time';
 // export { default as Form } from './form';
@@ -14,7 +15,19 @@ export { default as Table } from './table';
 export { default as Trigger } from './trigger';
 
 // 声明
-export { BubbleProps } from './bubble';
-export { ButtonProps } from './button';
+export type { BubbleProps } from './bubble';
+export type { ButtonProps } from './button';
+export type { ICheckValue, ICheckOption, CheckProps } from './check';
+export type { ProviderProps } from './config-provider';
+export type { DateTimeProps, PanelProps } from './date-time';
+// export type { FormProps } from './form';
+export type { InputProps } from './input';
+export type { LoadingProps } from './loading';
+export type { MessageParam } from './message';
+export type { PaginationProps } from './pagination';
+export type { ResizeObserverProps } from './resize-observer';
+export type { ISelection, ISelectOption, SelectProps } from './select';
+export type { ITableColumns, TableProps } from './table';
+export type { TriggerProps } from './trigger';
 
-export { popPosition, cssTextAlign } from './types';
+export type { popPosition, cssTextAlign } from './types';
