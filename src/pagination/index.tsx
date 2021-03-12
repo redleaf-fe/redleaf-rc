aad2efbb5b8f88e3a1b909673fa5fee9ff5d0bd0
zhufeng <vzhufeng@163.com>
2021-03-12 20:22:09 +0800

    fix(pagination): 公共types从types.ts导入

--- src/pagination/index.tsx
+++ src/pagination/index.tsx
@@ -14,6 +14,7 @@ import Select from '../select';
 import { IconEllipsis } from '../icon';
 import { prefixCls } from '../constants';
 import { between } from '../utils/js';
+import { baseProps } from '../types';
 
 import '../styles/common.less';
 import './style.less';
