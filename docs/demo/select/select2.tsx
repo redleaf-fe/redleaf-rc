77470c08e0de9e7dbfb62befb4f4240b452bc2de
zhufeng <vzhufeng@163.com>
2021-03-16 22:48:22 +0800

    docs(select): fix docs

--- docs/demo/select/select2.tsx
+++ docs/demo/select/select2.tsx
@@ -5,6 +5,15 @@ import '../../doc.less';
 
 const Select2 = () => {
   const [options, setOptions] = useState([]);
+
+  const getData = val => {
+    return new Promise(res => {
+      setTimeout(() => {
+        res([1, 2, 3, 4, 5].map(v => ({ text: v + val, value: v + val })));
+      }, 300);
+    });
+  };
+
   return (
     <>
       <div className="mb8">
@@ -13,11 +22,9 @@ const Select2 = () => {
           type="multi"
           options={options}
           onSearch={val => {
-            setTimeout(() => {
-              setOptions(
-                [1, 2, 3, 4, 5].map(v => ({ text: v + val, value: v + val })),
-              );
-            }, 300);
+            getData(val).then(res => {
+              setOptions(res);
+            });
           }}
           onChange={({ value, meta }) => {
             console.log(value, meta);
