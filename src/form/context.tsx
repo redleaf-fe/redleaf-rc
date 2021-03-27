86bc1656faaced5dbf33c9260bf20803ead072d4
zhufeng <vzhufeng@163.com>
2021-03-27 19:34:09 +0800

    feat(form): 增加相关声明

--- src/form/context.tsx
+++ src/form/context.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { baseProps } from '../types';
 
-export interface FormContextProps {
-  formRef?: {
-    values: any;
-    items: any;
-  };
+export interface IFormValues {
+  value: any;
+  name: string;
+  values: baseProps;
+}
+
+export interface IFormValidator {
+  rule: string | (({ value, name, values }: IFormValues) => boolean);
+  message: string;
+}
+
+export interface IFormRef {
+  values: baseProps;
+  items: baseProps;
+  validators: baseProps;
+}
+
+export interface IFormContext {
+  formRef?: IFormRef;
   layout?: 'horizontal' | 'vertical';
   onFormChange?: ({ name, value }: { name: string; value: any }) => void;
 }
 
-export const FormContext = React.createContext<FormContextProps>({});
+export const FormContext = React.createContext<IFormContext>({});
 
 export const ItemContext = React.createContext({});
