b022c325a8b3b0a275ebe55e7935b60deed250d9
zhufeng <vzhufeng@163.com>
2021-04-11 22:20:27 +0800

    docs(dialog): add demo

---
+++ docs/demo/dialog/dialog4.tsx
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react';
+import { Button, Dialog } from 'redleaf-rc';
+
+import { longText } from './data';
+import '../../doc.less';
+import './style.less';
+
+const Dialog4 = () => {
+  const close1 = useRef();
+
+  return (
+    <>
+      <Button
+        className="block mb8"
+        onClick={() => {
+          close1.current = Dialog.show({
+            content: (
+              <div>
+                <Button
+                  className="mt8"
+                  onClick={() => {
+                    Dialog.show({
+                      title: '另一个对话框',
+                      content: <div className="dialog4">{longText}</div>,
+                      showCloseIcon: true,
+                    });
+                  }}
+                >
+                  打开另一个对话框
+                </Button>
+              </div>
+            ),
+            title: '对话框中打开另一个对话框',
+            showCloseIcon: true,
+          });
+        }}
+      >
+        open
+      </Button>
+    </>
+  );
+};
+
+export default () => <Dialog4 />;
