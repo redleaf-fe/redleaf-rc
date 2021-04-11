c9b853c2be21f30fa3287d5bb4bcf52bf102f077
zhufeng <vzhufeng@163.com>
2021-04-11 22:16:16 +0800

    docs(dialog): fix doc

--- docs/demo/dialog/dialog3.tsx
+++ docs/demo/dialog/dialog3.tsx
@@ -1,12 +1,21 @@
 import React, { useRef, useState } from 'react';
 import { Button, Dialog } from 'redleaf-rc';
 
-import { longText } from './data';
+import Comp from './comp';
+
 import '../../doc.less';
 import './style.less';
 
 const Dialog3 = () => {
+  const [defaultValue, setDefaultValue] = useState({});
   const close1 = useRef();
+
+  const submit1 = ({ values }) => {
+    close1.current();
+    // 改变defaultValue
+    setDefaultValue(values);
+    console.log(values);
+  };
 
   return (
     <>
@@ -14,28 +23,23 @@ const Dialog3 = () => {
         className="block mb8"
         onClick={() => {
           close1.current = Dialog.show({
-            content: (
-              <div>
-                <Button
-                  className="mt8"
-                  onClick={() => {
-                    Dialog.show({
-                      title: '另一个对话框',
-                      content: <div className="dialog3">{longText}</div>,
-                      showCloseIcon: true,
-                    });
-                  }}
-                >
-                  打开另一个对话框
-                </Button>
-              </div>
-            ),
-            title: '对话框中打开另一个对话框',
+            content: <Comp submit={submit1} defaultValue={defaultValue} />,
+            title: '更新对话框中内容',
             showCloseIcon: true,
+            onClose: () => {
+              console.log('close');
+            },
           });
         }}
       >
         open
+      </Button>
+      <Button
+        onClick={() => {
+          setDefaultValue({ name: 'redleaf' });
+        }}
+      >
+        改变defaultValue
       </Button>
     </>
   );
