9bc813e6bd4235df5472523acb3605b7428b79e6
zhufeng <vzhufeng@163.com>
2021-04-23 21:30:48 +0800

    docs(menu): fix demo

--- docs/demo/menu/menu1.tsx
+++ docs/demo/menu/menu1.tsx
@@ -8,7 +8,24 @@ const Menu1 = () => (
     className="border-1px-black"
     datasets={[
       { value: '1', text: '111' },
-      { value: '2', text: '222' },
+      {
+        value: '2',
+        text: '222',
+        children: [
+          {
+            value: '3',
+            text: '333',
+            children: [
+              {
+                value: '7',
+                text: '777'
+              }
+            ]
+          }
+        ]
+      },
+      { value: '4', text: '444', children: [{ value: '6', text: '666' }] },
+      { value: '5', text: '555' }
     ]}
   />
 );
