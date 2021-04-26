931c6999150e13b198515db5725273ebfc42201b
zhufeng <vzhufeng@163.com>
2021-04-26 21:42:38 +0800

    feat(menu): menu dev

--- src/menu/index.tsx
+++ src/menu/index.tsx
@@ -11,7 +11,7 @@ import _uniq from 'lodash/uniq';
 
 import { prefixCls } from '../constants';
 import { baseProps } from '../types';
-import { toPlainArray } from '../utils/js';
+import { toPlainArray, deepFirstTraverse } from '../utils/js';
 import { IconArrowSingle } from '../icon';
 
 import './style.less';
@@ -25,6 +25,7 @@ import './style.less';
 5.高度不够时给滚动
 6.默认展开项
 7.只展开某一级的内容，其他的关闭只显示最顶层
+8.点击后展开当前项下面的全部子级
 
 */
 
@@ -48,56 +49,71 @@ const Menu = (props: MenuProps): ReactElement => {
   const { className, datasets = [], onChange, ...restProps } = props;
 
   const [activeItem, setActiveItem] = useState<baseProps>({});
+  // openId代表点击过的菜单项，showId代表展示的菜单项，展示的菜单项比点击的要多一级
   const [openId, setOpenId] = useState<number[]>([]);
+  const [showId, setShowId] = useState<number[]>([]);
 
   const menuData = useMemo(() => toPlainArray(datasets), [datasets]);
 
-  const renderItem = useCallback(
-    val => {
-      return (
-        <span
-          key={val.__id__}
-          className={cls(
-            `${prefixCls}-menu-item`,
-            `${prefixCls}-menu-item-indent-${val.__depth__}`,
-            {
-              [`${prefixCls}-menu-active-item`]:
-                val.__id__ === activeItem.__id__,
-              [`${prefixCls}-menu-item-hidden`]:
-                val.__depth__ !== 0 &&
-                !openId.includes(val.__id__) &&
-                !val.__parentId__.includes(val.__id__)
-            }
-          )}
-          onClick={() => {
-            if (val.children) {
-              const arr = _uniq(openId.concat(val.__id__));
-              setOpenId(
-                arr.concat(val.children.map((v: baseProps) => v.__id__))
-              );
+  const renderItem = useCallback(() => {
+    return menuData.map(val => (
+      <span
+        key={val.__id__}
+        className={cls(
+          `${prefixCls}-menu-item`,
+          `${prefixCls}-menu-item-indent-${val.__depth__}`,
+          {
+            [`${prefixCls}-menu-active-item`]: val.__id__ === activeItem.__id__,
+            [`${prefixCls}-menu-item-hidden`]:
+              val.__depth__ !== 0 &&
+              !showId.includes(val.__id__) &&
+              !val.__parentId__.includes(val.__id__)
+          }
+        )}
+        onClick={() => {
+          if (val.children) {
+            // 点击已展开的项，openId中要去除，showId中要保留
+            if (openId.includes(val.__id__)) {
+              // 当前点击项的所有后代项
+              const childrenId: number[] = [];
+              deepFirstTraverse(val.children, v => {
+                childrenId.push(v.__id__);
+              });
+              const arr = openId.filter(v => !childrenId.includes(v));
+              setOpenId(arr.filter(v => v !== val.__id__));
+              const arr2 = showId.filter(v => !childrenId.includes(v));
+              setShowId(arr2);
             } else {
-              setActiveItem(val);
+              const arr = openId.concat(val.__id__);
+              setOpenId(arr);
+              const arr2 = showId.concat(
+                val.children.map((v: baseProps) => v.__id__)
+              );
+              setShowId(arr2);
             }
-          }}
-        >
-          <span className={`${prefixCls}-menu-item-text`}>{val.text}</span>
-          {val.children && (
-            <svg
-              className={`${prefixCls}-menu-arrow-icon`}
-              viewBox="0 0 1024 1024"
-            >
-              <path transform="rotate(90,512,512)" d={IconArrowSingle} />
-            </svg>
-          )}
-        </span>
-      );
-    },
-    [activeItem, openId]
-  );
+          } else {
+            setActiveItem(val);
+          }
+        }}
+      >
+        <span className={`${prefixCls}-menu-item-text`}>{val.text}</span>
+        {val.children && (
+          <svg
+            className={`${prefixCls}-menu-arrow-icon`}
+            viewBox="0 0 1024 1024"
+          >
+            <path transform="rotate(90,512,512)" d={IconArrowSingle} />
+          </svg>
+        )}
+      </span>
+    ));
+  }, [activeItem, openId, showId, menuData]);
+
+  console.log(showId, openId);
 
   return (
     <span className={cls(`${prefixCls}-menu`, className)} {...restProps}>
-      {menuData.map((v, k) => renderItem(v, k))}
+      {renderItem()}
     </span>
   );
 };
