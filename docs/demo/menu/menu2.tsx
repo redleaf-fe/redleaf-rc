720d43fa608ce3c1125a9dde61f881f615b7fee1
zhufeng <vzhufeng@163.com>
2021-05-02 11:23:57 +0800

    docs(menu): fix demo

--- docs/demo/menu/menu2.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Menu } from 'redleaf-rc';
-
-import menuData from './data';
-import '../../doc.less';
-
-const Menu2 = () => <Menu className="border-1px-black" datasets={menuData} />;
-
-export default () => <Menu2 />;
