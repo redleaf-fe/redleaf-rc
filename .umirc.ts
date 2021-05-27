ed73439af55f0903504b247d67c45655829cbc4a
vzhufeng <vzhufeng@163.com>
2021-05-27 10:36:14 +0800

    chore: modify doc menu and history

--- .umirc.ts
+++ .umirc.ts
@@ -10,88 +10,92 @@ export default defineConfig({
   devServer: {
     port: pkg.port,
   },
-  base: '/rc/',
+  history: { type: 'hash' },
   favicon: 'https://gw.alicdn.com/imgextra/i3/O1CN01s3UDzQ1t5LdrmxVwF_!!6000000005850-2-tps-200-200.png',
   // more config: https://d.umijs.org/config,
-  menus: {
-    // 需要自定义侧边菜单的路径，没有配置的路径还是会使用自动生成的配置
-    '/components': [
-      {
-        title: '起步',
-        path: '起步',
-      },
-      {
-        title: '说明',
-        path: '说明',
-      },
-      {
-        title: '定制',
-        path: '定制',
-      },
-      {
-        title: 'bubble',
-        path: 'bubble',
-      },
-      {
-        title: 'button',
-        path: 'button',
-      },
-      {
-        title: 'check',
-        path: 'check',
-      },
-      {
-        title: 'config-provider',
-        path: 'config-provider',
-      },
-      {
-        title: 'date-time',
-        path: 'date-time',
-      },
-      {
-        title: 'dialog',
-        path: 'dialog',
-      },
-      {
-        title: 'form',
-        path: 'form',
-      },
-      {
-        title: 'input',
-        path: 'input',
-      },
-      {
-        title: 'loading',
-        path: 'loading',
-      },
-      {
-        title: 'menu',
-        path: 'menu',
-      },
-      {
-        title: 'message',
-        path: 'message',
-      },
-      {
-        title: 'pagination',
-        path: 'pagination',
-      },
-      {
-        title: 'resize-observer',
-        path: 'resize-observer',
-      },
-      {
-        title: 'select',
-        path: 'select',
-      },
-      {
-        title: 'table',
-        path: 'table',
-      },
-      {
-        title: 'trigger',
-        path: 'trigger',
-      },
-    ],
-  },
+  // menus: {
+  //   // 需要自定义侧边菜单的路径，没有配置的路径还是会使用自动生成的配置
+  //   '/components': [
+  //     {
+  //       title: '起步',
+  //       path: '起步',
+  //     },
+  //     {
+  //       title: '说明',
+  //       path: '说明',
+  //     },
+  //     {
+  //       title: '定制',
+  //       path: '定制',
+  //     },
+  //     {
+  //       title: 'bubble',
+  //       path: 'bubble',
+  //     },
+  //     {
+  //       title: 'button',
+  //       path: 'button',
+  //     },
+  //     {
+  //       title: 'check',
+  //       path: 'check',
+  //     },
+  //     {
+  //       title: 'config-provider',
+  //       path: 'config-provider',
+  //     },
+  //     {
+  //       title: 'date-time',
+  //       path: 'date-time',
+  //     },
+  //     {
+  //       title: 'dialog',
+  //       path: 'dialog',
+  //     },
+  //     {
+  //       title: 'form',
+  //       path: 'form',
+  //     },
+  //     {
+  //       title: 'input',
+  //       path: 'input',
+  //     },
+  //     {
+  //       title: 'loading',
+  //       path: 'loading',
+  //     },
+  //     {
+  //       title: 'menu',
+  //       path: 'menu',
+  //     },
+  //     {
+  //       title: 'message',
+  //       path: 'message',
+  //     },
+  //     {
+  //       title: 'pagination',
+  //       path: 'pagination',
+  //     },
+  //     {
+  //       title: 'resize-observer',
+  //       path: 'resize-observer',
+  //     },
+  //     {
+  //       title: 'select',
+  //       path: 'select',
+  //     },
+  //     {
+  //       title: 'table',
+  //       path: 'table',
+  //     },
+  //     {
+  //       title: 'tree',
+  //       path: 'tree',
+  //     },
+  //     {
+  //       title: 'trigger',
+  //       path: 'trigger',
+  //     },
+  //   ],
+  // },
 });
