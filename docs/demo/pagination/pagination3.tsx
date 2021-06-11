7345b122b5a3310a486b991facc536688cc94d44
vzhufeng <vzhufeng@163.com>
2021-06-11 10:57:29 +0800

    docs(pagination): fix demo

--- docs/demo/pagination/pagination3.tsx
+++ docs/demo/pagination/pagination3.tsx
@@ -32,6 +32,29 @@ const Pagination3 = () => {
           );
         }}
       />
+      <Pagination
+        className="block mb8"
+        totalItems={0}
+        renderTotalItems={({ totalItems, currentPage, pageSize, pages }) => {
+          return (
+            <span className="mr8">
+              共{totalItems}项数据，共{pages}页，每页{pageSize}项
+            </span>
+          );
+        }}
+      />
+      <Pagination
+        type="complex"
+        className="block mb8"
+        totalItems={0}
+        renderTotalItems={({ totalItems, currentPage, pageSize, pages }) => {
+          return (
+            <span className="mr8">
+              共{totalItems}项数据，共{pages}页，每页{pageSize}项
+            </span>
+          );
+        }}
+      />
       <Button
         onClick={() => {
           setItems(items + 9);
