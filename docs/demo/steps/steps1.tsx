264c577ffa92e24c56fbf32d329fa0ceea8d76fd
vzhufeng <vzhufeng@163.com>
2021-07-05 16:43:47 +0800

    docs(steps): fix demo

--- docs/demo/steps/steps1.tsx
+++ docs/demo/steps/steps1.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import { Steps } from 'redleaf-rc';
 
-import datasets from './data';
+import options from './data';
+import './style.less';
 import '../../doc.less';
 
-const Steps1 = () => <Steps datasets={datasets} />;
+const Steps1 = () => {
+  return (
+    <>
+      <Steps
+        options={[
+          { text: '11', value: '1' },
+          { text: '22', value: '2' },
+          { text: '33', value: '3' },
+          { text: '44', value: '4' },
+          { text: '55', value: '5' },
+          { text: '66', value: '6' }
+        ]}
+        className="block mb16"
+      />
+      <div className="mb16">设置初始值</div>
+      <Steps
+        options={options}
+        className="block mb16 step-horizontal"
+        defaultValue={'2'}
+      />
+      <Steps
+        options={options}
+        layout="vertical"
+        className="block step-vertical"
+        defaultValue={'2'}
+      />
+    </>
+  );
+};
 
 export default () => <Steps1 />;
