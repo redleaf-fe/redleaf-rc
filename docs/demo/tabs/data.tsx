d30053ebc858ed18fc92dc210a0fd6c43f83c12c
vzhufeng <vzhufeng@163.com>
2021-07-06 14:57:15 +0800

    docs(tabs): fix demo

--- docs/demo/tabs/data.tsx
+++ docs/demo/tabs/data.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Check } from 'redleaf-rc';
 
+import './style.less';
+
 export default [
   {
     text:
       '很长很长的文本很长很长的文本很长很长的文本很长很长的文本很长很长的文本很长很长的文本',
     value: '1',
     content: (
-      <div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+      <div className="content">
+        <Check
+          layout="vertical"
+          options={[
+            { value: 'option1', text: 'option1' },
+            { value: 'option2', text: 'option2' },
+            { value: 'option3', text: 'option3' }
+          ]}
+        />
       </div>
     )
   },
@@ -19,20 +26,28 @@ export default [
       '2222222222222222222222222222222222222222222222222222222222222222222222',
     value: '2',
     content: (
-      <Check
-        options={[
-          { value: 'option1', text: 'option1' },
-          { value: 'option2', text: 'option2' },
-          { value: 'option3', text: 'option3' }
-        ]}
-      />
+      <div className="content">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
     )
   },
-  { text: '33', value: '3' },
-  { text: 'disabled', value: '7', disabled: true },
+  {
+    text: '33',
+    value: '3',
+    content: (
+      <div className="content">
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+    )
+  },
   { text: '44', value: '4' },
   { text: '55', value: '5' },
   { text: '66', value: '6' },
+  { text: 'disabled', value: '7', disabled: true },
   { text: '77777', value: '8' },
   { text: '88888', value: '9' }
 ];
--- docs/demo/tabs/tabs1.tsx
+++ docs/demo/tabs/tabs1.tsx
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tabs } from 'redleaf-rc';
 
 import options from './data';
 import '../../doc.less';
 
 const Tabs1 = () => {
-  const [val, setVal] = useState('2');
   return (
-    <Tabs
-      options={options}
-      defaultValue="8"
-      value={val}
-      onChange={({ value, meta }) => {
-        console.log(value, meta);
-        setVal(value);
-      }}
-    />
+    <>
+      <Tabs options={options.slice(0, 3)} />
+    </>
   );
 };
 
