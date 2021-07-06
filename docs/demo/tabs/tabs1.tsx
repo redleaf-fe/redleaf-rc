2f7a73908c93abd79d355e40d7880a9d17526268
vzhufeng <vzhufeng@163.com>
2021-07-06 11:48:03 +0800

    docs(tabs): fix demo

--- docs/demo/tabs/tabs1.tsx
+++ docs/demo/tabs/tabs1.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tabs } from 'redleaf-rc';
 
-import datasets from './data';
+import options from './data';
 import '../../doc.less';
 
-const Tabs1 = () => <Tabs datasets={datasets} />;
+const Tabs1 = () => {
+  const [val, setVal] = useState('2');
+  return (
+    <Tabs
+      options={options}
+      defaultValue="8"
+      value={val}
+      onChange={({ value, meta }) => {
+        console.log(value, meta);
+        setVal(value);
+      }}
+    />
+  );
+};
 
 export default () => <Tabs1 />;
